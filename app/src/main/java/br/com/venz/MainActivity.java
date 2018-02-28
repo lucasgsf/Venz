3b65477da794142b79877940afb64c678c3d6e44
Lucas Ferreira <contatolucasgsf@gmail.com>
2018-02-28 00:36:49 -0300

    Versão 0.0.1

--- app/src/main/java/br/com/venz/MainActivity.java
+++ app/src/main/java/br/com/venz/MainActivity.java
@@ -2,12 +2,21 @@ package br.com.venz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private Toolbar mToolbarBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.tb_main);
+        mToolbar.setTitle("Venz");
+        //mToolbar.setLogo(R.drawable.ic_launcher_foreground);
+        //mToolbar.setSubtitle("Subtítulo");
+        setSupportActionBar(mToolbar);
     }
 }
